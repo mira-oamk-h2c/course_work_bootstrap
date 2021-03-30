d88a642d403d820747a438cfb50467709ac5c146
Mira Eerikäinen <c0eemi00@students.oamk.fi>
2021-03-30 10:19:40 +0300

    image gallery and fixes on responsivity

--- js/mycode.js
+++ js/mycode.js
@@ -1,23 +1,23 @@
 
 
 $(document).ready(function () {
-  
+
   // Quiz
-  
+
   let total = 0;
-  
+
   $(".form-check-input.quiz").click(function () {
-    
+
     let nameAttr = $(this).attr("name");
     let choice = "[name=" + nameAttr + "]";
     let visible = "#" + nameAttr;
-    
+
     // Disable radio buttons once one is selected
     $(choice).prop("disabled", true);
-    
+
     // Show the correct answer and explanation
     $(visible).removeClass("not_visible");
-    
+
     // Highlight the wrong or/and correct answer
     let answer = Number($(this).val());
     if (answer === 1) {
@@ -28,13 +28,13 @@ $(document).ready(function () {
       let correct = "[name=" + nameAttr + "][value=1]";
       $(correct).parent().addClass("right");
     }
-    
+
     $("#result").html("Your result: " + total + "/5 correct answers!");
-    
+
   });
-  
+
   // BMI
-  
+
   /**
   * Calculates the body mass index
   * @param {Number} weight   weight in kg
@@ -45,7 +45,7 @@ $(document).ready(function () {
     let result = (weight / Math.pow(height / 100, 2.5)) * 1.3;
     return result;
   }
-  
+
   /**
   * 
   * @param {Number} value    person's height in cm
@@ -57,21 +57,38 @@ $(document).ready(function () {
     limit = limit.toFixed(0);
     return limit;
   }
-  
+
   /**
   * Check that all the input data is given
   * @returns {Boolean} true >> ok, false >> not ok
   */
-  function validateInput(birthYear, weight, height) {
+  function validateInput(birthYear, weight, height, age) {
 
     if (birthYear === 0 || weight === 0 || height === 0) {
+      $("#m_title").html("Oops, it seems you missed something!");
+      $("#m_body").html("<p>" + "You need to fill all the fields:" + "</p>" +
+        "<p>" + "Birth year, Weight, Height" + "</p>");
+      let message = new bootstrap.Modal(document.getElementById("mymessage"),
+        { backdrop: "static" }
+      );
+      message.show();
       return false;
+    } else if (age < 20 || age > 60) {
+      $("#m_title").html("Your age is not in the range.");
+      $("#m_body").html("<p>" + "BMI results are valid for people from age 20 to 60." + "</p>");
+      let message = new bootstrap.Modal(document.getElementById("mymessage"),
+        { backdrop: "static" }
+      );
+      message.show();
+      return true;
+    } else {
+      return true;
     }
 
   }
-  
+
   $("#calculate_bmi").click(function () {
-    
+
     // Read input data & define variables
     let birthYear = Number($("#birth_year").val());
     let weight = Number($("#weight").val());
@@ -81,34 +98,17 @@ $(document).ready(function () {
     let age = year - birthYear;
 
     // Validate input data and bring out notification modal if not correct
-    
-    if (validateInput(birthYear, weight, height) === false) { 
-      $("#m_title").html("Oops, it seems you missed something!");
-      $("#m_body").html("<p>" + "You need to fill all the fields:" + "</p>" + 
-      "<p>" + "Birth year, Weight, Height" + "</p>");
-      let message = new bootstrap.Modal(document.getElementById("mymessage"),
-        { backdrop: "static" }
-      );
-      message.show();
-      return;
-    } 
-    
-    if (age < 20 || age > 60) {
-      $("#m_title").html("Your age is not in the range.");
-      $("#m_body").html("<p>" + "BMI results are for people from age 20 to 60." + "</p>");
-      let message = new bootstrap.Modal(document.getElementById("mymessage"),
-        { backdrop: "static" }
-      );
-      message.show();
+
+    if (validateInput(birthYear, weight, height, age) === false) {
       return;
     }
-    
+
     // Handle correct input data
     let result = getBMI(weight, height);
-    
+
     // Print to output element
     $("#result_bmi").html(result.toFixed(1));
-    
+
     // Show explanation based on result (bmi)
     if (result < 17) {
       $("#bmi_expl_1").addClass("bg-info");
@@ -123,42 +123,45 @@ $(document).ready(function () {
     } else if (result >= 40) {
       $("#bmi_expl_7").addClass("bg-info");
     }
-    
+
   });
-  
+
   // Show normal Weight Range if checkbox is selected
 
   $("#weight_range").click(function () {
-    
+
     if ($(this).prop("checked") === true) {
       $(this).prop("checked", true);
-      
+
       // Read input data
       let value = Number($("#height").val());
-      
+
       // Handle input data
       let lower = getWeightLimit(value, 18.5);
       let upper = getWeightLimit(value, 24.9);
-      
+
       // Print to output element
       $("#normal").html(lower + "–" + upper + " kg");
 
+
     } else $("#normal").html("");
-    
+
   });
-  
-  // Select and empty all output elements with click on input element "Weight"
+
+  // Select and empty all output elements with click on input element
   $(".bmi.form-control").focusin(function () {
     $(this).select();
     $("#result_bmi").html("");
     $("#normal").html("");
     $(".card-body").removeClass("bg-info");
+    $("#weight_range").prop("checked", false);
   });
-  
-  
+
+
   // Waist Control
+
   $("#calculate_waist").click(function () {
-    
+
     // Read input data
     let value = Number($("#waist").val());
     let gender = Number($("[name=flexRadioDefault]:checked").val());
@@ -167,15 +170,15 @@ $(document).ready(function () {
 
     if (value === 0) {
       $("#m_title").html("Oops, it seems you missed something!");
-      $("#m_body").html("<p>" + "You need to fill the field:" + "</p>" + 
-      "<p>" + "Waist" + "</p>");
+      $("#m_body").html("<p>" + "You need to fill the field:" + "</p>" +
+        "<p>" + "Waist" + "</p>");
       let message = new bootstrap.Modal(document.getElementById("mymessage"),
         { backdrop: "static" }
       );
       message.show();
       return;
     }
-    
+
     // Handle correct data based on gender selection
     if (gender === 1) {
       if (value < 90) {
@@ -195,24 +198,24 @@ $(document).ready(function () {
       }
     }
   });
-  
+
   // Select all and empty li elements with click on input element "Waist"
   $("#waist").focusin(function () {
     $(this).select();
     $(".list-group-item").removeClass("bg-info");
   });
-  
+
   // Focus on input element with click on radio button
   $("[name=flexRadioDefault]").click(function () {
     $("#waist").focus();
   });
-  
+
   // Pop-overs
   let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
   let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
   });
-  
-  
+
+
 });
 
